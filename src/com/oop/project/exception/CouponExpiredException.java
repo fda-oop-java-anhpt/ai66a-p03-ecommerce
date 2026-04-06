26913fb24324ef29d8f54b4b61d8c9c3c85cd039
duongbui0811 <buidangduong08112006@gmail.com>
2026-04-06 20:40:35 +0700

    Refactor CouponExpiredException class
    Removed redundant constructor and class definition.

--- src/com/oop/project/exception/CouponExpiredException.java
+++ src/com/oop/project/exception/CouponExpiredException.java
@@ -1,6 +1,4 @@
 package com.oop.project.exception;
-
-<<<<<<< HEAD
 /**
  * Thrown when a coupon is expired, inactive, does not exist,
  * or the order total does not meet the minimum required value (FR-4.2).
@@ -14,10 +12,4 @@ public class CouponExpiredException extends RuntimeException {
     public CouponExpiredException(String message, Throwable cause) {
         super(message, cause);
     }
-=======
-public class CouponExpiredException extends RuntimeException {
-    public CouponExpiredException(String message) {
-        super(message);
-    }
->>>>>>> 118f56a (Add files in exception)
 }

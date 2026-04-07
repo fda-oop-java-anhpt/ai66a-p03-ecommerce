25dfddb02bfc50cac890de8613187c7b8fb06a6d
Tuan Anh <nguyentuananh0608tuananh@gmail.com>
2026-04-07 23:07:48 +0700

    update repository layer

--- src/com/oop/project/repository/SystemSettingRepository.java
+++
@@ -1,8 +0,0 @@
-package com.oop.project.repository;
-
-import com.oop.project.model.SystemSetting;
-
-public interface SystemSettingRepository {
-    SystemSetting findByKey(String key);
-    boolean update(String key, String value);
-}

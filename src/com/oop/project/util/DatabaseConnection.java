9db51573d039830edd135d8a3d96ce65341d28ac
Thu Thu <anhthulengoc00@gmail.com>
2026-04-08 23:17:20 +0700

    luu lai env

--- src/com/oop/project/util/DatabaseConnection.java
+++ src/com/oop/project/util/DatabaseConnection.java
@@ -4,8 +4,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-
-
 public class DatabaseConnection {
     private static final String URL = System.getenv("DB_URL");
     private static final String USER = System.getenv("DB_USER");
